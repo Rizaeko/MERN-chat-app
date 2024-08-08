5515cdfa53f404e28fc2779d0ab89c980b8267a8
Your Name <basumataryjason@gmail.com>
2024-08-08 18:52:49 +0545

    backend completed

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+
+//package imports
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+//routes import
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from './db/connectToMongoDB.js'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT 
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+
+//Routes
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+// app.get("/",(req,res) =>{
+//     res.send("Server is ready")
+// })
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server Running on port ${PORT}`)
+})
